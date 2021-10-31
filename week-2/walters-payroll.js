02fe6d0509386a4e0d4ece08fdfae6371fb3ee92
shebelieves <shewalters@my365.bellevue.edu>
2021-10-31 13:56:42 -0400

    entered Ludwig java code

---
+++ week-2/walters-payroll.js
@@ -0,0 +1,12 @@
+//Title: Assignment 2.2
+// Author: Professor Krasso
+// Date: October, 31, 2021
+// Modified by: Shelaine Walters
+// Description: How to target elements on the html file with
+//              JavaScript to create a payroll chart.
+
+
+
+// First name variables defined as strings and binded to inner html by their specified id
+let firstNameLudwig = "Ludwig";
+document.getElementById("txtirstNameLudwig").innerHTML = firstNameLudwig;
