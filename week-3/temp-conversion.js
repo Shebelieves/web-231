122973756b345a4700c397f67f5d7c8dca717d98
shebelieves <shewalters@my365.bellevue.edu>
2021-11-03 16:36:01 -0400

    updated styling

--- week-3/temp-conversion.js
+++
@@ -1,4 +0,0 @@
-function temperatureConverter(valNum){
-    valNum = parseFloat(valNum);
-    document.getElementById("outputCelsius").innerHTML = ((parseFloat(fahrenheit) - 32) / 1.8.toFixed(2));
-}
